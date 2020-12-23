23e316d8291fb3ba8eb6b8fc28bb2727071996e0
Bảo <baoenzo@users.noreply.github.com>
2020-12-23 22:25:05 +0700

    Export

---
+++ FallenDoll/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace FallenDoll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(25, 57);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(105, 44);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(162, 57);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(105, 44);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose file TableTranslation.uexp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 114);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnImport);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fallen Doll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FallenDoll/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallenDoll
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "UEXP|*.uexp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string pathUexp = ofd.FileName;
+                BinaryReader rd = new BinaryReader(File.OpenRead(pathUexp));// rd file này // file uexp
+                BinaryWriter wt = new BinaryWriter(File.Create(pathUexp + "_new"));// m sẽ tạo file mới uexp_new
+                List<string> lstLines = File.ReadAllLines(Path.GetDirectoryName(ofd.FileName) + @"\dump.txt").ToList();
+                rd.BaseStream.Seek(0x0, SeekOrigin.Begin);
+                byte[] start = rd.ReadBytes(75);
+                wt.Write(start);
+                foreach (string item in lstLines)
+                {
+
+                    wt.Write(rd.ReadInt64());
+                    long c = rd.ReadInt64();
+                    string textTemp = item.Replace("[0]", "\0");
+                    if (c!= 22)
+                    {
+                        wt.Write(rd.ReadInt64());
+                        wt.Write(rd.ReadInt64());
+                        wt.Write(rd.ReadInt64());
+                    }
+                    else
+                    {
+                        if (item != "null" && IsChinese(textTemp))
+                        {
+                            byte[] txt = Encoding.Unicode.GetBytes(textTemp);
+                            wt.Write(txt.Length / -2);
+                            wt.Write(txt);
+                        }
+                    }
+                    break;
+
+                }
+
+                wt.Close();
+                rd.Close();
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "UEXP Unreal 4|*.uexp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string pathUE = @"C:\Users\Baoenzo\Desktop\1\TableTranslation.uexp";
+                string pathUA = @"C:\Users\Baoenzo\Desktop\1\TableTranslation.uasset";
+                List<string> temps = new List<string>();
+                FileStream fs = new FileStream(pathUE, FileMode.Open);
+                using (var reader = new BinaryReader(fs))
+                {
+                    reader.BaseStream.Seek(0x35, SeekOrigin.Begin);
+                    for (int i = 0; i < 1353; i++)
+                    {
+                        reader.ReadInt64(); //unk
+                        long t = reader.ReadInt64();
+                        if (t != 22)
+                        {
+                            reader.ReadInt64();
+                            reader.ReadInt64();
+                            reader.ReadInt64();
+                        }
+                        else
+                        {
+                            reader.ReadInt64();
+                        }
+                        //5 bytes
+                        reader.ReadByte();
+                        int sizeText = reader.ReadInt32();
+                        if (sizeText < 0)
+                        {
+                            sizeText = sizeText * -2;
+                            byte[] data = reader.ReadBytes(sizeText);
+                            string a = Encoding.Unicode.GetString(data).Replace("\0", "[0]");
+                            if (a.Length != 0)
+                                temps.Add(a);
+                            else
+                                temps.Add("null");
+                        }
+                        else
+                        {
+                            byte[] data = reader.ReadBytes(sizeText);
+                            string a = Encoding.Default.GetString(data).Replace("\0", "[0]");
+                            if (a.Length != 0)
+                                temps.Add(a);
+                            else
+                                temps.Add("null");
+                        }
+
+                    }
+                }
+
+                try
+                {
+                    File.WriteAllLines(Path.GetDirectoryName(ofd.FileName) + @"/dump.txt", temps);
+                    MessageBox.Show("Done!!", "Done!");
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        public bool IsChinese(string text)
+        {
+            return text.Any(c => (uint)c >= 0x4E00 && (uint)c <= 0x2FA1F);
+        }
+    }
+}
